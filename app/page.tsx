c71efee4ca06cad54b9348503ae0a4cc27bb5af8
Kelly Chen <kellychen966@gmail.com>
2024-06-23 15:25:06 -0400

    Cache test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-'use client'
+"use client";
 
 export default function Home() {
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to PillPals Test</h1>
+      <h1>Welcome to PillPals</h1>
     </main>
   );
 }
